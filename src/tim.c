8c56f964a8e637f9d2aac4b1e2df3ec758634380
Miles J. Litteral <Mandaloe2@gmail.com>
2026-03-19 01:01:07 -0400

    Refactor: Remove unused Tim structures and enums
    Removed unused structures and enums related to Tim image processing.

--- src/tim.c
+++ src/tim.c
@@ -3,52 +3,7 @@
 #include <stdexcept>
 #include <fstream>
 #include <iostream>
-
-struct TimId {
-    uint8_t id;
-    uint8_t version;
-    uint16_t reservedSpace;
-};
-
-enum PixelMode {
-    CLUT4 = 0,
-    CLUT8 = 1,
-    Direct15 = 2,
-    Direct24 = 3,
-    Mixed = 4
-};
-
-struct TimFlags {
-    PixelMode pmode;
-    uint8_t cf;
-    uint32_t reservedSpace;
-};
-
-struct Colour {
-    uint8_t r, g, b;
-    uint8_t stp;
-};
-
-struct TimClut {
-    uint32_t bnum;
-    uint16_t dx, dy, w, h;
-    std::vector<Colour> clut;
-};
-
-typedef Colour PixelEntry; // For simplicity, 15/24-bit pixels will be stored as Colour
-
-struct TimPixelData {
-    uint32_t bnum;
-    uint16_t dx, dy, w, h;
-    std::vector<PixelEntry> data;
-};
-
-struct TimImage {
-    TimId id;
-    TimFlags flags;
-    TimClut clut;             // Only if cf == 1
-    TimPixelData pixels;
-};
+#include "tim.h"
 
 // ----------------------
 // ByteReader helper
